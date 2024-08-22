39078e883bf1b03bb6f981604b8a162feab9c544
PremveerSingh195 <prem.veer2836@gmail.com>
2024-08-22 23:54:02 +0530

    dsghv

--- src/index.js
+++ src/index.js
@@ -6,8 +6,37 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/' , (req,res) => {
-    res.send('Hello world')
+app.get('/jokes' , (req,res) => {
+
+ const jokes = [
+    {
+        "id": 1,
+        "joke" : "joke1",
+        "description" : "description1"
+    },
+    {
+        "id": 2,
+        "joke" : "joke2",
+        "description" : "description2"
+    },
+    {
+        "id": 3,
+        "joke" : "joke3",
+        "description" : "description3"
+    },
+    {
+        "id": 4,
+        "joke" : "joke4",
+        "description" : "description4"
+    },
+    {
+        "id": 5,
+        "joke" : "joke5",
+        "description" : "description5"
+    }
+   ]
+
+    res.send(jokes)
 })
 
 app.listen(PORT , () => {
